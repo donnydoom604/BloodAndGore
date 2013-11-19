d3d77e8f89c120450d946cb040dfa0df814cc499
MarinusLeeuwerik <m.leeuwerik@outlook.com>
2013-11-19 19:25:56 +0100

    Making a start.

---
+++ src/code/marinusleeuwerik/BloodAndGore/BloodAndGore.java
@@ -0,0 +1,23 @@
+package code.marinusleeuwerik.BloodAndGore;
+
+import java.util.logging.Logger;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BloodAndGore extends JavaPlugin implements Listener {
+	private static final Logger log = Logger.getLogger("Minecraft");
+	public static BloodAndGore plugin;
+	
+	@Override
+	public void onDisable() {
+		log.info("");
+	}
+	
+	@Override
+	public void onEnable() {
+		log.info("");
+	}
+	
+	
+}
